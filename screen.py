504dfd52207bc4939eb677e0e2f87a8aeee830bd
Chrunch-tech <uday014@gmail.com>
2021-02-24 06:23:48 +0530

    Add dropdown widget to the editor screen

--- screen.py
+++ screen.py
@@ -10,6 +10,7 @@ from kivy.uix.screenmanager import Screen
 from tkinter import *
 from tkinter import filedialog
 from automate import Automate
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class Base(Screen):
@@ -30,7 +31,7 @@ class Base(Screen):
     def open_file_manager(self, instance):
         """
         Open the file manager
-        :param instance: Determine where the click click on the (MDFloatingActionButtonSpeedDial)
+        :param instance: Determine where the click happen on the (MDFloatingActionButtonSpeedDial)
         :return: None
         """
         if instance.icon == "assets/images/add.png":
@@ -49,10 +50,9 @@ class Base(Screen):
         :return: None
         """
         automate = Automate(filename)
-        workbook_data = automate.get_workbook_data()
+        self.workbook_data = automate.get_workbook_data()
         self.screen_transition_editor()
-        self.manager.render_wb_data(render_data=workbook_data)
-        # print(workbook_data)
+        self.manager.render_wb_data(render_data=self.workbook_data)
 
     def screen_transition_editor(self):
         """
@@ -61,6 +61,49 @@ class Base(Screen):
         """
         self.manager.transition.direction = "left"
         self.manager.current = "editor_screen"
+
+    def toolbar_menu_sheets(self):
+        """
+        Defining the menu bar for the MDDropDownItem/sheets ->
+        (Which give the information about the sheets).
+        :return: None
+        """
+        sheets = self.workbook_data["sheets"]
+        drop_down_menu = MDDropdownMenu(
+            caller=self.ids.drop_item_sheets,
+            items=[{"text": str(sheets[i]), "font_name": "assets/font/Heebo-Regular/ttf"}
+                    for i in range(len(sheets))
+                   ],
+            width_mult=4
+        )
+        drop_down_menu.bind(on_release=self.drop_down_menu_callback)
+        drop_down_menu.open()
+
+    def toolbar_menu_tools(self):
+        """
+        Defining the menu bar for the MDDropDownItem/tools ->
+        (Which give the information about the tools).
+        :return: None
+        """
+        automation_tool = ["Apply formulas", "Sort", "Reverse", "Delete", "Merge sheet"]
+        drop_down_menu = MDDropdownMenu(
+            caller=self.ids.drop_item_tools,
+            items=[{"text": str(automation_tool[i]), "font_name": "assets/font/Heebo-Regular/ttf"}
+                   for i in range(len(automation_tool))
+                   ],
+            width_mult=4
+        )
+        drop_down_menu.bind(on_release=self.drop_down_menu_callback)
+        drop_down_menu.open()
+
+    def drop_down_menu_callback(self, instance_menu, instance_menu_item):
+        """
+        Receive the input from the mentioned drop_down_menu
+        :param instance_menu: Instance of the menu on which users click.
+        :param instance_menu_item: Instance of the menu_item on which users click.
+        :return: None
+        """
+        pass
 
 
 class HomeScreen(Base):
@@ -79,7 +122,7 @@ class EditorScreen(Base):
     def back_to_home(self):
         """
         Define back to home screen functionality
-        :return: None
+        :return: 
         """
         self.manager.transition.direction = "right"
         self.manager.current = "home_screen"
