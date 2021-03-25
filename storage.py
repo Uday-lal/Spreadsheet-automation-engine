0ee22b63a253e25a61e552f95819901c8a55c02d
Chrunch-tech <uday014@gmail.com>
2021-03-25 10:16:32 +0530

    Update storage.py -> its objective is to make wb data somewhere at the center

--- storage.py
+++ storage.py
@@ -6,38 +6,45 @@ company: UR's tech.ltd
 --------------------------------------------------------->
 
 ~~~~~~~~~~~~~~~~~~~~~~~
-Making the storage object and and enabling the
-live storing the feature.
+Making the storage object to make wb data
+some where at the center
 """
 from kivy.storage.jsonstore import JsonStore
 from datetime import datetime
 import os
+import json
 
 
 class Storage(JsonStore):
     def __init__(self, **kwargs):
-        super(Storage, self).__init__(**kwargs)
+        super(Storage, self).__init__(**kwargs, filename="Propoint.json")
+        self.filename = "Propoint.json"
+        self.current_date = datetime.today().strftime("%d-%m-%Y")
+        save_path = os.path.join(os.path.expanduser("~"), "AppData\\Roaming\\Propoint")
+
         try:
-            self.filename = "Propoint.json"
-            self.current_date = datetime.today().strftime("%d-%m-%Y")
-            save_path = os.path.join(os.path.expanduser("~"), "AppData\\Roaming\\Propoint")
             os.mkdir(save_path)
-            os.chdir(save_path)
         except FileExistsError:
             pass
+
+        os.chdir(save_path)
 
     def save(self, wb_data, filename):
         """
         Saving the given data
+        :param filename: Name of the workbook
         :param wb_data: Data need to be save
         :return: None
         """
-        self.put(key=filename, date_of_modify=self.current_date, data=wb_data)
+        path, _filename = os.path.split(filename)
+        self.put(key=_filename, path=path, date_of_modify=self.current_date, data=wb_data)
 
-    def read_data(self, filename):
+    def read_data(self):
         """
         Read the stored data
-        :param filename: Name of the workbook
         :return: list
         """
-        return self.get(key=filename)["data"]
+        file = open(self.filename)
+        data = json.load(file)
+        for key in data:
+            return data[key]["data"]
