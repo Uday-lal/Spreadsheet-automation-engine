88852d70541a224eecb5a3c9c89bd57476cdbd65
Chrunch-tech <uday014@gmail.com>
2021-03-29 19:38:09 +0530

    Complete the coc engine

--- Automate/coc_engine.py
+++ Automate/coc_engine.py
@@ -10,6 +10,7 @@ COC(Coordinate Operation Controller) is type engine which is
 responsible for handle all the inputs coming from input box
 to select cells to perform mathematical logic
 """
+import re
 
 char_table = {
     'a': 'A',
@@ -60,32 +61,58 @@ class CoordinateOperationController:
         :return: dict
         """
         data_for_execution = {}
-        shaped_input = self.shape_input()
+        shape_input = self.shape_input()
+        root_key = "new"
+        operator_coming_times = 0
+        if "=" in shape_input:
+            root_key = self.data_index(command=[shape_input[0]])[0]
+            shape_input = shape_input[2:len(shape_input)]
+        data_for_execution[root_key] = []
 
-    def data_index(self, sample_input):
+        for i, command in enumerate(shape_input):
+            operator = self.operators[command] if command in self.operators else None
+
+            if operator is not None:
+                operator_coming_times += 1
+                node = {}
+                if operator_coming_times == 1:
+                    last_value, next_value = self.data_index([shape_input[i - 1]])[0], self.data_index(
+                        [shape_input[i + 1]])[0]
+                    node[operator] = (last_value, next_value)
+                    data_for_execution[root_key].append(node)
+                else:
+                    next_value = self.data_index(command=[shape_input[i + 1]])[0] if not shape_input[
+                        i + 1].isdigit() else \
+                        shape_input[i + 1]
+                    node[operator] = next_value
+                    data_for_execution[root_key].append(node)
+
+        return data_for_execution
+
+    def data_index(self, command):
         """
-        Understanding the input and based on that
-        raise several actions
+        Returns the index of each command
+        or input presented in the headers
 
         --------------------------------
         sample input: ["B", "*", "C"]
         --------------------------------
-        :param sample_input: Input data base on which it going to return some value
+        :param command: Input data base on which it going to return some value
         :return: list
         """
-        headers = self.headers["rows"][0]
+        headers = self.headers
 
         return_data = []
 
-        for value in sample_input:
+        for value in command:
             for header in headers:
-                if len(value) < 2:
+                if len(value) == 1:
                     if value in header:
                         return_data.append(headers.index(header))
                 else:
-                    row_index, column_index = value
+                    row_index, column_index = re.match(r"([a-z]+)([0-9]+)", value, re.I).groups()
                     if row_index in header:
-                        return_data.append((headers.index(header), column_index))
+                        return_data.append((headers.index(header), int(column_index)))
 
         return return_data
 
