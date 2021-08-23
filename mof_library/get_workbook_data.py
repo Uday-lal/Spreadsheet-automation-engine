0a9d0a3ecf0106cdd3ba6d4c04e2cd3161d4862c
Uday-lal <76507514+Uday-lal@users.noreply.github.com>
2021-08-23 15:28:30 +0530

    Update get_workbook_data.py

--- mof_library/get_workbook_data.py
+++ mof_library/get_workbook_data.py
@@ -1,10 +1,4 @@
 """
---------------------------------------------------------->
-Copyrights (c) to UR's tech.ltd 2021. All rights reserved
-Author: Uday lal
-company: UR's tech.ltd
---------------------------------------------------------->
-
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Getting workbook data with the help of
 my MoF(Max of four) algorithm
